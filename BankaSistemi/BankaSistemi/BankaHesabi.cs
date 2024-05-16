808a7879562545fde0f1223b71e14ad9953ef44e
UgurElma <154337639+UgurElma@users.noreply.github.com>
2024-05-16 11:42:20 +0300

    Banka Sistemi
    Basit bir şekilde; para yükleme, çekme, transfer ve hesap özeti çıkarma işlemlerini yapan bir C# konsol tabanlı bir kodlamadır.

---
+++ BankaSistemi/BankaSistemi/BankaHesabi.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+
+class BankaHesabi
+{
+    int hesabNumarasi { get; set; }
+    string hesabIsmı { get; set; }
+    float hesabBakiye { get; set; }
+    public BankaHesabi(int ID, string adi, float bakiye)
+    {
+        hesabNumarasi = ID;
+        hesabIsmı = adi;
+        hesabBakiye = bakiye;
+        BankaHesaplari.bankaHesabiList.Add(this);
+    }
+
+    public void HesapOzeti()
+    {
+        Console.WriteLine("Hesap Numarası: " + hesabNumarasi + "\nHesap İsmi: " + hesabIsmı + "\nHesap Bakiyesi: " + hesabBakiye + "\nGeri dönmek için klavyeden bir karaktere dokunun!");
+        Console.ReadKey();
+    }
+
+    public void ParaEklemeIslemi(float miktar)
+    {
+        if (miktar > 0)
+        {
+            hesabBakiye += miktar;
+            Console.WriteLine("Para başarıyla hesabınıza eklendi.");
+        }
+        else
+            Console.WriteLine("Lütfen, pozitif miktar giriniz");
+    }
+    public void ParaÇekmeIslemi(float miktar)
+    {
+        if ((miktar > 0) && (hesabBakiye - miktar >= 0))
+        {
+            hesabBakiye -= miktar;
+            Console.WriteLine("Para başarıyla hesabınıza çekildi.");
+        }
+        else if (hesabBakiye - miktar < 0)
+            Console.WriteLine("Yetersiz bakiye!");
+        else
+            Console.WriteLine("Lütfen, pozitif miktar giriniz");
+    }
+    public void ParaTransferi(float miktar, int ID)
+    {
+        bool varMi = false;
+        BankaHesabi arananKan = null;
+        for (int i = 0; i < BankaHesaplari.bankaHesabiList.Count; i++)
+        {
+            if (ID == BankaHesaplari.bankaHesabiList[i].hesabNumarasi)
+            {
+                varMi = true;
+                arananKan = BankaHesaplari.bankaHesabiList[i];
+                break;
+            }
+        }
+        if ((miktar > 0) && (hesabBakiye - miktar >= 0) && varMi)
+        {
+            hesabBakiye -= miktar;
+            arananKan.hesabBakiye += miktar;
+            Console.WriteLine("Para transferi başarıyla gerçekleşti.");
+        }
+        else if (!varMi)
+        {
+            Console.WriteLine("Kullanıcı uyumsuzluğu!");
+        }
+        else
+        {
+            Console.WriteLine("Para transferi gerçekleştirilemedi.");
+        }
+    }
+    public void All()
+    {
+        List<BankaHesabi> bankaHesabis = BankaHesaplari.bankaHesabiList;
+        for (int i = 0; i < bankaHesabis.Count; i++)
+        {
+            Console.Write(bankaHesabis[i].hesabNumarasi + "\n" + bankaHesabis[i].hesabIsmı + "\n" + bankaHesabis[i].hesabBakiye + "\n");
+        }
+    }
+}
---
+++ BankaSistemi/BankaSistemi/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        BankaHesabi bankaHesabi = new BankaHesabi(1111, "ADMİN", 0);
+        BankaHesabi yeniBankaHesabi = new BankaHesabi(0, null, 0);
+        Console.WriteLine("-------------- BANKA UYGULAMASINA HOŞ GELDİNİZ ----------------");
+        while (true)
+        {
+            Console.WriteLine("Giriş yapabilmek için hesap numaranızı ve adınızı girmelisiniz!");
+            try
+            {
+                Console.WriteLine("Hesap Numarası ve Hesap Adınız:");
+                yeniBankaHesabi = new BankaHesabi(int.Parse(Console.ReadLine()), Console.ReadLine(), 0);
+                break;
+            }
+            catch { Console.WriteLine("Lütfen, sadece sayi giriniz"); continue; }
+        }
+        BankaHesaplari.bankaHesabiList.Remove(BankaHesaplari.bankaHesabiList[1]);
+        while (true)
+        {
+            Console.Clear();
+            Console.WriteLine("Para Ekleme ---> +      Para Çekme ---> -      Para Transferi ---> =      Hesap Özeti ---> ?      Tüm Hesap Özeti ---> *\nYapmak istediğiniz işlemi seçin");
+            char islem = Console.ReadKey().KeyChar;
+            Console.Clear();
+            if (islem == '+')
+            {
+                while (true)
+                {
+                    Console.WriteLine(">>> Para Ekleme İşlemi <<<\n----------------------------");
+                    Console.Write("Yüklenecek miktarı giriniz: ");
+                    try
+                    {
+                        yeniBankaHesabi.ParaEklemeIslemi(float.Parse(Console.ReadLine()));
+                        Console.WriteLine("Geri dönmek için klavyeden bir karaktere dokunun!");
+                        Console.ReadKey();
+                        break;
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.ToString()); Console.ReadKey(); Console.Clear(); }
+                }
+            }
+            else if (islem == '-')
+            {
+                while (true)
+                {
+                    Console.WriteLine(">>> Para Çekme İşlemi <<<\n----------------------------");
+                    Console.Write("Çekilecek miktarı giriniz: ");
+                    try
+                    {
+                        yeniBankaHesabi.ParaÇekmeIslemi(float.Parse(Console.ReadLine()));
+                        Console.WriteLine("Geri dönmek için klavyeden bir karaktere dokunun!");
+                        Console.ReadKey();
+                        break;
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.ToString()); Console.ReadKey(); Console.Clear(); }
+                }
+            }
+            else if (islem == '=')
+            {
+                while (true)
+                {
+                    Console.WriteLine(">>> Para Transfer İşlemi <<<\n----------------------------");
+                    Console.WriteLine("Transfer edilecek miktarı ve Transfer edilecek hesap numarasını giriniz: ");
+                    try
+                    {
+                        yeniBankaHesabi.ParaTransferi(float.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+                        Console.WriteLine("Geri dönmek için klavyeden bir karaktere dokunun!");
+                        Console.ReadKey();
+                        break;
+                    }
+                    catch (Exception ex) { Console.WriteLine(ex.ToString()); Console.ReadKey(); Console.Clear(); }
+                    break;
+                }
+            }
+            else if (islem == '?')
+            {
+                Console.WriteLine(">>> Hesap Özeti İşlemi <<<\n----------------------------");
+                yeniBankaHesabi.HesapOzeti();
+            }
+            else if (islem == '*')
+            {
+                Console.WriteLine(">>> Tüm Hesap Özeti <<<\n----------------------------");
+                yeniBankaHesabi.All();
+                Console.WriteLine("Geri dönmek için klavyeden bir karaktere dokunun!");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Program kapatılacak emin misiniz? Hayır --> 0  Evet --> 'Herhangi bir tuşa basınız'");
+                if (Console.ReadKey().KeyChar != '0')
+                    break;
+            }
+        }
+    }
+}
+class BankaHesaplari
+{
+    public static List<BankaHesabi> bankaHesabiList { get; set; } = new List<BankaHesabi>();
+
+    public BankaHesaplari() { }
+}
